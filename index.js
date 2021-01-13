92f96bec8c75025b5f3818566dfe4657c03de8c9
Wesley <wesley.duff@charter.com>
2021-01-12 20:07:13 -0700

    update

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-    page : "" + fs.readFileSync(path(__dirname, './build/todo.txt')) + ""
+    page : "" + fs.readFileSync(path.join(__dirname, './build/todo.txt')) + ""
 };
